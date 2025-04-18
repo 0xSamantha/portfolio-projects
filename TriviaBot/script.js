5428641f8affb137dbf62d3e86b335ebadcdc6e2
samantha <103228045+0xSamantha@users.noreply.github.com>
2025-04-18 13:06:00 -0700

    init - trivia bot

---
+++ TriviaBot/script.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+ let botName = "TriviaBot";
+ let botLocation = "Neptune";
+ let favoriteLanguage = "Python";
+
+ console.log("My name is " + botName + " and I live on " + botLocation + ".");
+ console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+ let codingFact = favoriteLanguage + " is one of the main languages used in Machine Learning/AI!"
+ console.log(codingFact);
+
+ codingFact = "The name " + favoriteLanguage + " actually comes from Monty Python rather than the " + favoriteLanguage + " snake."
+ console.log(codingFact);
+
+ codingFact = favoriteLanguage + " is also one of the official programming languages at Google!"
+ console.log(codingFact);
+
+ console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
